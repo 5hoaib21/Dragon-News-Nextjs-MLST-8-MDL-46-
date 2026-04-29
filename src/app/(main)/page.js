475aa4540bd71a20385947b5136a99df858c844b
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-29 22:02:30 +0600

    Load News Data & Filter News Based On Category

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,35 +1,6 @@
-import Image from "next/image";
-
-
-
-const getCategories =async () => {
-const  res = await fetch('https://openapi.programming-hero.com/api/news/categories')
-const data = await res.json()
-return data.data;
-}
-
-
-export default async function Home() {
-  const categories = await getCategories()
-  console.log(categories.news_category, 'categories');
-  return (
-  <div className="grid grid-cols-12 gap-5 container mx-auto my-12">
-  <div className=" col-span-3">
-  <h2 className="font-bold text-xl">
-
-  All Categories
-  </h2>
-  <ul className="flex flex-col gap-3 mt-5">
-
-  {
-    categories.news_category.map(category =>{
-      return <li key={category.category_id} className="bg-slate-100 p-2 rounded-md font-bold text-center text-md">{category.category_name}</li>
-    } )
-  }
-  </ul>
-  </div>
-  <div className="font-bold text-3xl bg-purple-400 col-span-6">All News</div>
-  <div className="font-bold text-3xl bg-zinc-400 col-span-3"> Social Icons</div>
-  </div>
-  );
-}
+import { redirect } from "next/navigation";
+const defaultCategoryId = '01'
+const Home = async () => {
+  redirect(`/category/${defaultCategoryId}`)
+};
+export default Home;
---
+++ src/components/homepage/news/RightSidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+
+const RightSidebar = () => {
+  return (
+    <div>
+      <h2 className="font-bold text-xl mb-5">Login with</h2>
+      <div className="flex flex-col gap-3">
+        <button className="btn border-blue-500 text-blue-500">
+          <FaGoogle /> Login with Google
+        </button>
+        <button className="btn">
+          <FaGithub /> Login with GitHub{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RightSidebar;
